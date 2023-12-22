bdfc5366b8826fadd8d94e18df8f2cf74ed8e111
shivganesh <shivmarkad99@gmail.com>
2023-12-22 17:36:51 +0530

    Employee Attendance

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,19 @@
 public class Employee {
     public static void main(String[] args)
-    {
+    {  //Master
         System.out.println("Employee Wage Computation");
+        /* UC1-Check Employee is Present or Absent
+        - Use ((RANDOM)) for Attendance Check*/
+        int ISFULLTIME=1; //CONSTANTS
+        double  emp_Check=Math.floor(Math.random()*10 % 2);
+        if(emp_Check == ISFULLTIME)
+        {
+            System.out.println("Employee is present");
+        }
+        else
+        {
+            System.out.println("Employee is absent");
+        }
+
     }
 }
